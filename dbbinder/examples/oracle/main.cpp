a2e0811618197ebe2300b224669741af164dff57
xvjau <xvjau@274fa6bc-5dae-448f-a6eb-2890cbf5d362>
2008-08-28 18:25:32 +0000

    Created seperate examples for Firebird, MySQL and Oracle.
    git-svn-id: https://dbbinder.svn.sourceforge.net/svnroot/dbbinder/trunk@52 274fa6bc-5dae-448f-a6eb-2890cbf5d362

---
+++ dbbinder/examples/oracle/main.cpp
@@ -0,0 +1,58 @@
+/*
+    Copyright 2008 Gianni Rossi
+
+    This file is part of DBBinder++.
+
+    DBBinder++ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DBBinder++ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DBBinder++.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <list>
+
+#include "test.h"
+
+using namespace std;
+using namespace TestApp;
+	
+int main()
+{
+	{
+		cout << "Test 1\n------\nName:\t\tFestival:\n";
+		
+		selFestival stmt(2, 0);
+		
+		for( selFestival::iterator it = stmt.begin(); it != stmt.end(); ++it )
+		{
+			cout << it->getName() << "\t\t"
+					<< it->getFestival() << endl;
+		}
+	}
+
+	cout << "\n\nTest 2\n------\nName:\t\tFestival:\n";
+	
+	{
+		selFestival stmt(1, 0);
+		
+		typedef std::list<selFestival::row> MyListType;
+		MyListType myList;
+		
+		myList.assign( stmt.begin(), stmt.end() );
+		
+		for( MyListType::iterator it = myList.begin(); it != myList.end(); ++it )
+		{
+			cout << (*it)->getName() << "\t"
+					<< (*it)->getFestival() << endl;
+		}
+	}
+}
