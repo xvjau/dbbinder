f0f09523221859de4a684ff348e5ce815697dc2d
xvjau <xvjau@274fa6bc-5dae-448f-a6eb-2890cbf5d362>
2008-08-29 17:53:20 +0000

    More Firebird work.
    git-svn-id: https://dbbinder.svn.sourceforge.net/svnroot/dbbinder/trunk@56 274fa6bc-5dae-448f-a6eb-2890cbf5d362

--- dbbinder/firebirdgenerator.cpp
+++ dbbinder/firebirdgenerator.cpp
@@ -22,6 +22,8 @@
 namespace DBBinder
 {
 
+static const int DEFAULT_COL_COUNT = 10;
+	
 template<typename P, typename T> void add_dbd_param(P *&_dpb, T _param)
 {
 	*_dpb++ = _param;
@@ -53,10 +55,44 @@ inline bool checkFBError( const ISC_STATUS *_status )
 	}
 	return false;
 }
+
+SQLTypes fbtypeToSQLType(const int _fbtype)
+{
+		switch( _fbtype & ~1 )
+		{
+			case SQL_TEXT:
+			case SQL_VARYING:
+				return stText;
+			case SQL_SHORT:
+			case SQL_LONG:
+				return stInt;
+			case SQL_FLOAT:
+				return stFloat;
+			case SQL_DOUBLE:
+			case SQL_D_FLOAT:
+				return stDouble;
+			case SQL_TIMESTAMP:
+				return stTimeStamp;
+			case SQL_BLOB:
+			case SQL_ARRAY:
+				return stText;
+			case SQL_QUAD:
+				return stInt64;
+			case SQL_TYPE_TIME:
+				return stTime;
+			case SQL_TYPE_DATE:
+				return stDate;
+			case SQL_INT64:
+				return stInt64;
+			default:
+				return stText;
+		}
+}
 	
 FirebirdGenerator::FirebirdGenerator()
  : AbstractGenerator(), m_conn(0)
 {
+	m_dbengine = "firebird";
 }
 
 
@@ -78,9 +114,7 @@ bool FirebirdGenerator::checkConnection()
 		char *p = dpb;
 		
 		#define READ_PARAM(NAME) \
-		String NAME = m_dbParams[# NAME].value; \
-		if ( NAME.empty() ) \
-			FATAL( "MySQL: '" # NAME "' db parameter is empty." );
+		String NAME = m_dbParams[# NAME].value;
 		
 		READ_PARAM(db);
 		READ_PARAM(user);
@@ -107,18 +141,22 @@ bool FirebirdGenerator::checkConnection()
 
 String FirebirdGenerator::getBind(const ListElements::iterator& _item, int _index)
 {
+	return "";
 }
 
 String FirebirdGenerator::getReadValue(const ListElements::iterator& _item, int _index)
 {
+	return "";
 }
 
 void FirebirdGenerator::addSelect(SelectElements _elements)
 {
+	checkConnection();
+	
 	ISC_STATUS		err[32];
 	isc_tr_handle	tr = 0;
 	isc_stmt_handle	stmt = 0;
-	XSQLDA			*buffInput = 0, *buffOutput = 0;
+	XSQLDA			*buffOutput = 0;
 	
 	// Start Transaction
 	{
@@ -130,16 +168,40 @@ void FirebirdGenerator::addSelect(SelectElements _elements)
 	// statement	
 	isc_dsql_alloc_statement2( err, &m_conn, &stmt );
 	checkFBError( err );
+
+	buffOutput = (XSQLDA *)malloc( XSQLDA_LENGTH( DEFAULT_COL_COUNT ) );
+	memset(buffOutput, 0, XSQLDA_LENGTH( DEFAULT_COL_COUNT ));
+	buffOutput->version = SQLDA_VERSION1;
+	buffOutput->sqln = DEFAULT_COL_COUNT;
 	
 	isc_dsql_prepare( err, &tr, &stmt, _elements.sql.length(), const_cast<char*>(_elements.sql.c_str()), 3, buffOutput );
 	checkFBError( err );
+
+	// Check to see if the buffer is big enough
+	if ( buffOutput->sqld > buffOutput->sqln )
+	{
+		// Resize and re-describe
+		buffOutput = (XSQLDA *)realloc( buffOutput, XSQLDA_LENGTH( buffOutput->sqld ) );
+		buffOutput->sqln = buffOutput->sqld;
+		memset(buffOutput, 0, XSQLDA_LENGTH( buffOutput->sqld ));
+		isc_dsql_describe_bind( err, &stmt, SQLDA_VERSION1, buffOutput );
+	}
+
+	XSQLVAR *p = buffOutput->sqlvar;
+	for( int i = 0; i < buffOutput->sqld; ++i, p++ )
+	{
+		_elements.output.push_back( SQLElement( p->aliasname, fbtypeToSQLType( p->sqltype ), i, p->sqllen ));
+	}
+
+	//Free, deallocate and rollback everything
+	
+	free(buffOutput);
 	
 	isc_dsql_free_statement( err, &stmt, DSQL_drop );
 	checkFBError( err );
 	
 	isc_rollback_transaction( err, &tr );
 	checkFBError( err );
-			
 	
     AbstractGenerator::addSelect(_elements);
 }
