4743e779aa0fb6397cc40be390d99fc27621aab3
xvjau <xvjau@274fa6bc-5dae-448f-a6eb-2890cbf5d362>
2009-08-17 14:33:37 +0000

    Created base files for CMake includes.
    git-svn-id: https://dbbinder.svn.sourceforge.net/svnroot/dbbinder/trunk@83 274fa6bc-5dae-448f-a6eb-2890cbf5d362

---
+++ dbbinder/extra/CMake/FindDBBinder.cmake
@@ -0,0 +1,8 @@
+FIND_PROGRAM(DBBINDER_EXECUTABLE NAMES dbbinder)
+
+#Need to add these:
+#XXX_VERSION_STRING      A human-readable string containing the version of the package found, if any.
+#XXX_VERSION_MAJOR       The major version of the package found, if any.
+#XXX_VERSION_MINOR       The minor version of the package found, if any.
+#XXX_VERSION_PATCH       The patch version of the package found, if any.
+#XXX_INCLUDE_DIRS        The final set of include directories listed in one variable for use by client code.  This should not be a cache entry.
