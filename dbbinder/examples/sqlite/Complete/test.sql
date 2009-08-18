2d8b01231b8588017501a90dd2bff8ecea7d187c
xvjau <xvjau@274fa6bc-5dae-448f-a6eb-2890cbf5d362>
2009-08-18 14:45:47 +0000

    Made SQL less verbose on error as a workaround while CMakeLists' target dependencies don't work correctly.
    git-svn-id: https://dbbinder.svn.sourceforge.net/svnroot/dbbinder/trunk@94 274fa6bc-5dae-448f-a6eb-2890cbf5d362

--- dbbinder/examples/sqlite/Complete/test.sql
+++ dbbinder/examples/sqlite/Complete/test.sql
@@ -1,4 +1,4 @@
-create table "USERS"
+create table if not exists "USERS"
 (
 	"IDUSER"	integer not null primary key,
 	"NAME"		text,
@@ -6,12 +6,12 @@ create table "USERS"
 	"PASSWORD"	text
 );
 
-insert into "USERS" values(1,'Aphrodite','aphrodite@sex.gr','ganymede');
-insert into "USERS" values(2,'Venus','venus@sky.com','titan');
-insert into "USERS" values(3,'Athena','athena@crea.org.br','222222');
-insert into "USERS" values(4,'Ceres','ceres@agriculture.europa.eu','quirinalis');
+insert or ignore into "USERS" values(1,'Aphrodite','aphrodite@sex.gr','ganymede');
+insert or ignore into "USERS" values(2,'Venus','venus@sky.com','titan');
+insert or ignore into "USERS" values(3,'Athena','athena@crea.org.br','222222');
+insert or ignore into "USERS" values(4,'Ceres','ceres@agriculture.europa.eu','quirinalis');
 
-create table "FESTIVALS"
+create table if not exists "FESTIVALS"
 (
 	"IDFESTIVAL"	integer not null primary key,
 	"TITLE"			text,
@@ -19,20 +19,20 @@ create table "FESTIVALS"
 	"DATE"			integer
 );
 
-insert into "FESTIVALS" values (1,'Aphrodisiac','Athina',20987);
-insert into "FESTIVALS" values (2,'Ambarvalia','Colle Aventino',38342);
+insert or ignore into "FESTIVALS" values (1,'Aphrodisiac','Athina',20987);
+insert or ignore into "FESTIVALS" values (2,'Ambarvalia','Colle Aventino',38342);
 
-create table "USERS_FESTIVALS"
+create table if not exists "USERS_FESTIVALS"
 (
 	"IDUSER"		integer not null,
 	"IDFESTIVAL"	integer not null,
 	primary key("IDUSER", "IDFESTIVAL")
 );
 
-insert into "USERS_FESTIVALS" values (1, 1);
-insert into "USERS_FESTIVALS" values (2, 1);
-insert into "USERS_FESTIVALS" values (3, 1);
-insert into "USERS_FESTIVALS" values (4, 1);
-insert into "USERS_FESTIVALS" values (2, 2);
-insert into "USERS_FESTIVALS" values (3, 2);
-insert into "USERS_FESTIVALS" values (4, 2);
+insert or ignore into "USERS_FESTIVALS" values (1, 1);
+insert or ignore into "USERS_FESTIVALS" values (2, 1);
+insert or ignore into "USERS_FESTIVALS" values (3, 1);
+insert or ignore into "USERS_FESTIVALS" values (4, 1);
+insert or ignore into "USERS_FESTIVALS" values (2, 2);
+insert or ignore into "USERS_FESTIVALS" values (3, 2);
+insert or ignore into "USERS_FESTIVALS" values (4, 2);
